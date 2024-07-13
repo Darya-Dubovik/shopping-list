a9c88ded14cfd39c2b9a78f7f03f81f5720f10ed
Darya-Dubovik <dashka_ivashenko@mail.ru>
2024-07-13 17:34:20 +0300

    Домашнее задание 4
    Создала список покупок

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
+const input = document.querySelector('#input');
+const items = document.querySelector('#item');
+const pointContainer = document.querySelector('.groceries');
+
+input.addEventListener('keyup', function(e) {
+    if (e.keyCode === 13) {
+
+    const pointText = input.value;
+
+    const newPoint = document.createElement('p');
+    newPoint.classList.add('item');
+    newPoint.textContent = pointText;
+
+    if (pointText != '') {
+        pointContainer.append(newPoint);
+    }
+  
+    const elements = document.querySelectorAll('.item'); 
+    for (let item of elements) {
+        item.addEventListener('click', function () {
+        item.classList.toggle('done');
+        })
+    }
+
+    input.value = '';
+
+    }
+});
+
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
